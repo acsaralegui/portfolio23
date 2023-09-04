888ac8b25f6309a0f8f818e091b38ae9736b23e6
Aaron Saralegui <aaron.saralegui@gmail.com>
2023-09-04 12:49:27 -0700

    change opt

--- webpack.prod.js
+++ webpack.prod.js
@@ -15,7 +15,6 @@ module.exports = merge(common, {
   },
   optimization: {
     minimizer: [
-      new OptimizeCssAssetsPlugin(),
       new TerserPlugin(),
       new HtmlWebpackPlugin({
         template: './src/template.html',
